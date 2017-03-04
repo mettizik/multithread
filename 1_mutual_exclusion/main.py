a3064d18183f90c9ef2e7631375854e1a3c66705
metizik <mokych.apriorit@gmail.com>
2017-03-05 00:14:00 +0200

    Fixed to pep8

--- 1_mutual_exclusion/main.py
+++ 1_mutual_exclusion/main.py
@@ -2,19 +2,26 @@
 import CustomThread
 import CriticalResource
 
+
 def incr(resource):
     resource.update(resource.value() + 1)
+
 
 def decr(resource):
     resource.update(resource.value() - 1)
 
+
 if __name__ == "__main__":
     res = CriticalResource.SharedResource(0)
     threads = []
-    for i in range(10):        
-        threads.append(CustomThread.CustomThread(i + 1, "Incrementation thread #{}".format(i), res, incr))
-    for i in range(10, 20):        
-        threads.append(CustomThread.CustomThread(i + 1, "Decrementation thread #{}".format(i), res, decr))
+    for i in range(10):
+        threads.append(
+            CustomThread.CustomThread(
+                i + 1, "Incrementation thread #{}".format(i), res, incr))
+    for i in range(10, 20):
+        threads.append(
+            CustomThread.CustomThread(
+                i + 1, "Decrementation thread #{}".format(i), res, decr))
 
     for thread in threads:
         thread.start()
